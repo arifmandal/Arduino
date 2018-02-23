2b740b07e98f52776a334feecd12371417c84e9b
arifmandal <32813976+arifmandal@users.noreply.github.com>
2018-02-24 00:30:37 +0200

    Add files via upload

---
+++ dc.ino
@@ -0,0 +1,18 @@
+int motorPin=7;//pinlerimizi tanimladik
+int ledPin=13;
+
+void setup() {
+  Serial.begin(9600);//serial monutoru baslattik
+  pinMode(motorPin,OUTPUT);//cikis pinlerimizi atadik.
+  pinMode(ledPin,INPUT);
+}
+
+void loop() {
+  int potDeger=analogRead(A0);//degeri okuduk
+  int yeniDeger=map(potDeger,0,1023,0,255);//map ile 0-255 arasindaki bir degere donusturduk
+
+  Serial.println(yeniDeger);//degeri gormek icin yazdirdik
+  delay(250);//gecikme payi 
+  digitalWrite(ledPin,potDeger);//ledimizi pot degerine gore yaktik
+  analogWrite(motorPin,yeniDeger);//motoru ise yeni degerimize gore calistirdik.
+}
