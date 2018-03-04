570a6ecbbc06d6ff9790cd3421605b897a9cebfc
arifmandal <32813976+arifmandal@users.noreply.github.com>
2018-03-04 20:07:39 +0200

    Add files via upload

---
+++ Ldr_Kullanimi.ino
@@ -0,0 +1,25 @@
+int ldrPin = A0;
+int ldrDeger;
+int ledPin = 6;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+
+}
+
+void loop() {
+  ldrDeger = analogRead(ldrPin);
+  Serial.println(ldrDeger);
+
+  if (ldrDeger > 600) {
+    digitalWrite(ledPin, HIGH);
+
+  } else {
+    digitalWrite(ledPin, LOW);
+
+
+  }
+  delay(250);
+
+}
