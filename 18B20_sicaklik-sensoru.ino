49e49a90bd987dc1ea22bd12c6550d26e9058b6d
arifmandal <32813976+arifmandal@users.noreply.github.com>
2018-03-24 23:29:57 +0200

    Add files via upload

---
+++ 18B20_sicaklik-sensoru.ino
@@ -0,0 +1,30 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define DATA_PIN 2
+int led1 = 13;
+int led2 = 12;
+OneWire oneWire(DATA_PIN);
+DallasTemperature sensors(&oneWire);
+void setup(void)
+{
+  Serial.begin(9600);
+  sensors.begin();
+}
+void loop(void)
+{
+  sensors.requestTemperatures();
+  Serial.print("Oda sicakligi: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.println(" derece");
+  delay(1000);
+  if (sensors.getTempCByIndex(0) > 30) {
+    digitalWrite(led2, LOW);
+    digitalWrite(led1, HIGH);
+  } else {
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+
+
+  }
+}
