3e227dc8cd34687bcb57ae847b2c35f70b54884c
arifmandal <32813976+arifmandal@users.noreply.github.com>
2018-04-12 18:27:20 +0300

    Add files via upload

---
+++ Water_Sensor.ino
@@ -0,0 +1,36 @@
+int sensorPin = A0;
+int ledRed = 12;
+int ledGreen = 13;
+int buzzerPin = 11;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledRed, OUTPUT);
+   pinMode(sensorPin, INPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = analogRead(sensorPin);
+  Serial.println(value);
+  float hesaplananDeger = (4.0 / 390.0) * value;
+  Serial.print(hesaplananDeger);
+  Serial.println(" cm");
+  delay(500);
+  if (hesaplananDeger >= 3) {
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(buzzerPin, HIGH);
+
+  } else {
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledRed, LOW);
+    digitalWrite(buzzerPin, LOW);
+  }
+
+}
