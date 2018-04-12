cb22354ed954f868458a588d9ae04465c63e37ff
arifmandal <32813976+arifmandal@users.noreply.github.com>
2018-04-12 18:25:40 +0300

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,36 @@
+int kirmiziLed=13;
+int sariLed=12;
+int yesilLed=8;
+int maviLed=7;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(kirmiziLed,OUTPUT);
+  pinMode(sariLed,OUTPUT);
+  pinMode(yesilLed,OUTPUT);
+  pinMode(maviLed,OUTPUT);
+  digitalWrite(kirmiziLed,HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(kirmiziLed,HIGH);
+  delay(50);
+  digitalWrite(kirmiziLed,LOW);
+  delay(50);
+  digitalWrite(sariLed,HIGH);
+  delay(50);
+  digitalWrite(sariLed,LOW);
+  delay(50);
+  digitalWrite(yesilLed,HIGH);
+  delay(50);
+  digitalWrite(yesilLed,LOW);
+  delay(50);
+  digitalWrite(maviLed,HIGH);
+  delay(50);
+  digitalWrite(maviLed,LOW);
+  delay(50);
+
+}
---
+++ anahtar.ino
@@ -0,0 +1,42 @@
+int kirmiziLed = 13;
+int sariLed = 12;
+int yesilLed = 11;
+int anahtar1 = 2;
+int anahtar2 = 3;
+int anahtar3 = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(kirmiziLed, OUTPUT);
+  pinMode(sariLed, OUTPUT);
+  pinMode(yesilLed, OUTPUT);
+  pinMode(anahtar1, INPUT);
+  pinMode(anahtar2, INPUT);
+  pinMode(anahtar3, INPUT);
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value1 = digitalRead(anahtar1);
+  int value2 = digitalRead(anahtar2);
+  int value3 = digitalRead(anahtar3);
+  if (value1 == 1) {
+    digitalWrite(kirmiziLed, HIGH);
+  } if (value2 == 1) {
+    digitalWrite(kirmiziLed, HIGH);
+    digitalWrite(sariLed, HIGH);
+
+  }  if (value3 == 1) {
+    digitalWrite(kirmiziLed, HIGH);
+    digitalWrite(sariLed, HIGH);
+    digitalWrite(yesilLed, HIGH);
+  } else {
+    digitalWrite(kirmiziLed, LOW);
+    digitalWrite(sariLed, LOW);
+    digitalWrite(yesilLed, LOW);
+  }
+
+}
