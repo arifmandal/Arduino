b6cbaf91a71107e00debbc284d3fce2011bc9da7
arifmandal <32813976+arifmandal@users.noreply.github.com>
2018-05-26 14:30:51 +0300

    Add files via upload

---
+++ FullSweep.ino
@@ -0,0 +1,22 @@
+#include <X113647Stepper.h>
+
+static const int STEPS_PER_REVOLUTION = 64 * 32;  // change this to fit the number of steps per revolution for your motor
+
+// initialize the stepper library on pins 8 through 11:
+X113647Stepper myStepper(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
+
+void setup() {
+  // set the speed in rpm:
+  myStepper.setSpeed(20);
+}
+
+void loop() {
+  // step one revolution  in one direction:
+  myStepper.step(STEPS_PER_REVOLUTION);
+  delay(500);
+
+  // step one revolution in the other direction:
+  myStepper.step(-STEPS_PER_REVOLUTION);
+  delay(500);
+}
+
---
+++ Keypad.ino
@@ -0,0 +1,66 @@
+#include<Keypad.h>
+#include<Password.h>
+
+const byte satir = 4;
+const byte sutun = 4;
+int counter=0;
+Password password = Password( "0000" );
+char tusTakimi[satir][sutun] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+
+};
+byte satirPin[satir] = {9, 8, 7, 6};
+byte sutunPin[sutun] = {13, 12, 11, 10};
+Keypad pad = Keypad(makeKeymap(tusTakimi), satirPin, sutunPin, satir, sutun);
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Sifrenizi giriniz... ");
+  pad.addEventListener(keypadEvent);
+}
+
+void loop() {
+  pad.getKey();
+  
+}
+void keypadEvent(KeypadEvent eKey) {
+  
+  switch (pad.getState()) {
+    case PRESSED:
+    if(eKey=='*'){
+        Serial.println(" Kontrol Ediliyor...");
+        }else if(eKey=='#'){
+          Serial.print(counter+2);
+          Serial.println(". kez sifrenizi giriyorsunuz...");
+          }
+        
+        else{
+     
+      Serial.print(eKey);
+      
+        }
+      switch (eKey) {
+        case '*':
+          checkPassword();
+          break;
+        case '#':
+          password.reset();
+          counter++;
+          break;
+        default:
+          password.append(eKey);
+          break;
+      }
+  }
+}
+void checkPassword() {
+  if (password.evaluate()) {
+    Serial.println("Sifre dogru, giris yapabilirsiniz... ");
+    
+  } else {
+    Serial.println("Sifre HATALI # tusuna basarak tekrar deneyiniz... ");
+  }
+}
+
